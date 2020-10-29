e6e10fa5d369dada70de94a121acb8552e31739c
NikolaiK <kornienkokolya23@gmail.com>
2020-10-29 12:33:38 +0300

    Lesson1

---
+++ Client - Server/Assets/Scripts/Items/EquipmentItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New equipment", menuName = "Inventory/Equipment")]
+public class EquipmentItem : Item {
+
+    public EquipmentSlotType EquipSlot;
+
+    public int DamageModifier;
+    public int ArmorModifier;
+    public int SpeedModifier;
+
+    public override void Use(Player player) {
+        player.Inventory.RemoveItem(this);
+        EquipmentItem oldItem = player.Equipment.EquipItem(this);
+        if (oldItem != null) player.Inventory.AddItem(oldItem);
+        base.Use(player);
+    }
+
+    public virtual void Equip(Player player)
+    {
+        if (player != null)
+        {
+            UnitStats unitStats = player.Character.UnitStats;
+            unitStats.Damage.AddModifier(DamageModifier);
+            unitStats.Armor.AddModifier(ArmorModifier);
+            unitStats.MoveSpeed.AddModifier(SpeedModifier);
+        }
+    }
+
+    public virtual void Unequip(Player player)
+    {
+        if (player != null)
+        {
+            UnitStats unitStats = player.Character.UnitStats;
+            unitStats.Damage.RemoveModifier(DamageModifier);
+            unitStats.Armor.RemoveModifier(ArmorModifier);
+            unitStats.MoveSpeed.RemoveModifier(SpeedModifier);
+        }
+    }
+}
+
+public enum EquipmentSlotType { Head, Chest, Legs, RighHand, LeftHand }
---
+++ Client - Server/Assets/Scripts/LoginIU.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoginIU : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _loginPanel;
+
+    private void Start()
+    {
+        if ((NetworkManager.singleton as MyNetworkManager).serverMode) _loginPanel.SetActive(false);
+    }
+
+    public void Login()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+}
